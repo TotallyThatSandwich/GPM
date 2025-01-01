f631db0946e620f9be555910c7a186e58759302a
ThatSandwich <hamishheath2008@gmail.com>
2025-01-01 16:15:02 +1100

    basic command parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[derive(Debug)]
+struct Args {
+    #[arg(short, long)]
+    verbose: bool,
+    
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    ///install a specific package
+    Install {
+        ///name of package to install, defults to latest release, to specify
+        ///version add @V:[version] after package name, e.g. `locate_zahirs_dad@V:[1.0.1]`
+        package: String
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+  
+    println!("{:?}", args);
 }
